22231cf20f1ed282ac16630774a5f048d192a78c
ritesh <rieshdhapatepatil@gmail.com>
2024-09-04 14:00:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {Html5QrcodeScanner} from "html5-qrcode"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
+  const [result, setResult] = useState(null);
   useEffect(() => {
     function onScanSuccess(decodedText, decodedResult) {
+      setResult(decodedText);
       console.log(`Code scanned = ${decodedText}`, decodedResult);
     }
     var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", {
@@ -16,6 +18,7 @@ function App() {
   return (
     <div className="App">
       <div id="qr-reader" style={{ width: "600px" }}></div>
+      <h1>{ result }</h1>
     </div>
   );
 }
